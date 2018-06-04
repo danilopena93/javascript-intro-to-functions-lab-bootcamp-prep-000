dba7eb9cce9214e1420c3c20e4fe425a70a4d580
danilopena@utexas.edu <danilopena@utexas.edu>
2018-06-04 22:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  else if (string.toUpperCase() == string){
+  else if (string.toUpperCase() === string){
     return "YES INDEED!"
   }
-  console.log(string.toLowerCase())
+  else (string === "I love you, Grandma.")
+    return "I love you too."
 }
